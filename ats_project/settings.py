c7e34dfee2587c41cd90f1bc85e008a7e14e6747
Mohitkumar Talreja <62738399+mathematical-guy@users.noreply.github.com>
2024-09-17 23:39:18 +0530

    Added Postgres Support

--- ats_project/settings.py
+++ ats_project/settings.py
@@ -67,8 +67,12 @@ WSGI_APPLICATION = 'ats_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config.get('NAME', ""),
+        'USER': config.get('USER', ""),
+        'PASSWORD': config.get('PASSWORD', ""),
+        'HOST': config.get('HOST', ""),
+        'PORT': config.get('PORT', ""),
     }
 }
 
